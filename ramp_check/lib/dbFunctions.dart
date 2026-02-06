06a116136b5efcacab180e981d0f1a83e9792ce0
Holt570 <30250524@blackpool.ac.uk>
2026-02-06 00:10:07 +0000

    Added getJobs function

--- ramp_check/lib/dbFunctions.dart
+++ ramp_check/lib/dbFunctions.dart
@@ -4,24 +4,23 @@ import 'package:http/http.dart' as http;
 class WarehouseApi {
   WarehouseApi();
 
-  String baseUrl = "http://localhost:5000";
-
   static const apiKey = 'api_warehouse_student_key_1234567890abcdef';
 
-  Map<String, String> _jsonHeaders() => {
+  static Map<String, String> _jsonHeaders() => {
         'Content-Type': 'application/json',
       };
 
-  Map<String, String> _authHeaders() => {
+  static Map<String, String> _authHeaders() => {
         ..._jsonHeaders(),
         'X-API-Key': apiKey,
       };
 
-  Future<Map<String, dynamic>> createLog({
+  static Future<Map<String, dynamic>> createLog({
     required String title,
     required String comment,
     required String status,
   }) async {
+    String baseUrl = "http://localhost:5000";
     final response = await http.post(
       Uri.parse('$baseUrl/sync/jobs'),
       headers: _authHeaders(),
@@ -32,5 +31,15 @@ class WarehouseApi {
       }),
     );
     return jsonDecode(response.body) as Map<String, dynamic>;
+  }
+
+  static Future<List<Map<String, dynamic>>> getJobs(
+  ) async {
+    String baseUrl = "http://localhost:5000";
+    final response = await http.get(
+      Uri.parse('$baseUrl/sync/jobs'),
+      headers: _authHeaders(),
+    );
+    return List<Map<String, dynamic>>.from(jsonDecode(response.body));
   }
 }
